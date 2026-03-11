a38e5c1edeca746b1865d00afc21fb31e6b4fc10
junganthonyy <junganthonyy@gmail.com>
2026-03-11 13:53:42 -0700

    Adding Duplicate Cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -60,6 +60,10 @@ export async function createEditCabin(newCabin, id) {
     throw new Error(`Cabin could not be created.`);
   }
 
+  if (hasImagePath) {
+    return data;
+  }
+
   // 2 Upload the image
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
