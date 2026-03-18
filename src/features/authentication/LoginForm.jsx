575bea6e855bc24235b15fa15b63fd69c3a4a446
junganthonyy <junganthonyy@gmail.com>
2026-03-17 17:04:22 -0700

    Update Header

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("test@test.com");
-  const [password, setPassword] = useState("password");
+  const [email, setEmail] = useState("test@email.com");
+  const [password, setPassword] = useState("123123123");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
